9fa05b1a963658017ef6b1fc99245860c7f8629b
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-05-27 23:30:48 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -12,5 +12,7 @@ source venv/bin/activate  # Pada Windows: venv\Scripts\activate
 # Instal dependensi dari requirements.txt
 pip install --upgrade pip  # Memperbarui pip
 pip install -r requirements.txt
+pip install xgboost
+
 
 
