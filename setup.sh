837e3501ffaa7dbb4f97ca5127791babbc332b84
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-05-27 20:20:47 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ python -m venv venv
 source venv/bin/activate  # Pada Windows: venv\Scripts\activate
 
 # Instal dependensi dari requirements.txt
+pip install --upgrade pip  # Memperbarui pip
 pip install -r requirements.txt
 
-pip install numpy==1.23.3
 
