544b09fe9e7b7bfb98d609f0172dc7168d466a13
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-05-27 11:25:11 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+# Hapus environment virtual yang ada
+rm -rf venv
+
+# Buat environment virtual baru
+python -m venv venv
+
+# Aktifkan environment virtual
+source venv/bin/activate  # Pada Windows: venv\Scripts\activate
+
+# Instal dependensi dari requirements.txt
 pip install -r requirements.txt
+
