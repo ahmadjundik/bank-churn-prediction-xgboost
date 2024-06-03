b8075ec6a35843ceb417284066508d624c29e3f4
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-06-03 19:36:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ def main():
     st.title("Bank Customer Churn Prediction and Visualization")
     image = Image.open('churn bank prediction.png')
     st.image(image)
+    
     st.header("**Pendahuluan**")
     latar_belakang = """
+    <div style='text-align: justify'>
     Dalam era digital, industri perbankan menghadapi peningkatan churn, yaitu 
     kehilangan pelanggan ke bank lain, akibat persaingan ketat, perubahan preferensi 
     konsumen, dan teknologi yang memudahkan perbandingan layanan. Churn ini mengurangi 
@@ -15,18 +17,22 @@ def main():
     CRM menekankan pentingnya hubungan efektif dengan pelanggan untuk kesuksesan bisnis. 
     Fokus kami adalah memprediksi churn menggunakan model machine learning untuk mengurangi kehilangan 
     nasabah dan mempertahankan pelanggan berharga.
+    </div>
     """
-    st.write(latar_belakang)
+    st.markdown(latar_belakang, unsafe_allow_html=True)
+    
     st.header("**Dataset dan Machine Learning**")
     dataset = """
+    <div style='text-align: justify'>
     Dataset yang kami gunakan berasal dari sumber data Kaggle. Kami memilih dataset 
-    bernama “ Bank Churn Data Exploration And Churn Prediction ”.
+    bernama “Bank Churn Data Exploration And Churn Prediction”.
     Dan untuk Machine Learning nya kami memakai Machine Learning
     bernama XGBoost.
+    </div>
     """
-    st.write(dataset)
-
+    st.markdown(dataset, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
+
 
