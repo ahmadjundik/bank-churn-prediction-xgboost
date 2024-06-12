9c2f4e102c49ab22b18c4819d570d111a2443d91
Ahmad Jundi <154776571+ahmadjundik@users.noreply.github.com>
2024-06-12 23:22:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,10 @@ def main():
     st.header("**Pendahuluan**")
     latar_belakang = """
     <div style='text-align: justify'>
-    Dalam era digital, industri perbankan menghadapi peningkatan churn, yaitu 
-    kehilangan pelanggan ke bank lain, akibat persaingan ketat, perubahan preferensi 
-    konsumen, dan teknologi yang memudahkan perbandingan layanan. Churn ini mengurangi 
-    laba dan memerlukan prediksi tepat untuk retensi pelanggan. Pelanggan adalah aset penting, 
-    sehingga menjaga loyalitas melalui Customer Relationship Management (CRM) menjadi krusial. 
-    CRM menekankan pentingnya hubungan efektif dengan pelanggan untuk kesuksesan bisnis. 
-    Fokus kami adalah memprediksi churn menggunakan model machine learning untuk mengurangi kehilangan 
-    nasabah dan mempertahankan pelanggan berharga.
+    Dalam era digital, industri perbankan menghadapi peningkatan churn, yaitu kehilangan pelanggan ke bank lain, akibat persaingan ketat, perubahan preferensi 
+    konsumen, dan teknologi yang memudahkan perbandingan layanan. Churn ini mengurangi laba dan memerlukan prediksi tepat untuk retensi pelanggan. Pelanggan adalah aset penting, 
+    sehingga menjaga loyalitas melalui Customer Relationship Management (CRM) menjadi krusial. CRM menekankan pentingnya hubungan efektif dengan pelanggan untuk kesuksesan bisnis. 
+    Fokus kami adalah memprediksi churn menggunakan model machine learning untuk mengurangi kehilangan nasabah dan mempertahankan pelanggan berharga.
     </div>
     """
     st.markdown(latar_belakang, unsafe_allow_html=True)
@@ -25,9 +21,7 @@ def main():
     st.header("**Dataset dan Machine Learning**")
     dataset = """
     <div style='text-align: justify'>
-    Dataset yang kami gunakan berasal dari sumber data Kaggle. Kami memilih dataset 
-    bernama “Bank Churn Data Exploration And Churn Prediction”.
-    Dan untuk Machine Learning nya kami memakai Machine Learning
+    Dataset yang kami gunakan berasal dari sumber data Kaggle. Kami memilih dataset bernama “Bank Churn Data Exploration And Churn Prediction”. Dan untuk Machine Learning nya kami memakai Machine Learning
     bernama XGBoost.
     </div>
     """
